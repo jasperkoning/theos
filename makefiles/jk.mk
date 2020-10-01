a0ec12652e7d0c635897e1588596e010462def97
U-NLSGM\JKoning <JKoning@SWDEV-SPDEC2018.schut.com>
2020-10-01 15:29:39 +0200

    update jk.mk

--- makefiles/jk.mk
+++ makefiles/jk.mk
@@ -1,10 +1,14 @@
-JK_VENDOR = $(THEOS)/vendor
 JK_SDK = $(THEOS)/sdks/iPhoneOS13.5.sdk
+JK_LIB = /usr/local/lib
+JK_INCLUDE = /usr/local/include
+
+CC = clang -isysroot $(JK_SDK)
+CXX = clang++ -isysroot $(JK_SDK)
+
+JK_VENDOR = $(THEOS)/vendor
 JK_SYSROOT = -isysroot $(JK_SDK)
 JK_CLANG = clang $(JK_SYSROOT)
 JK_CLANG++ = clang++ $(JK_SYSROOT)
-JK_LIB = /usr/local/lib
-JK_INCLUDE = /usr/local/include
 JK_LSUBSTRATE = -L$(JK_VENDOR)/lib,-lsubstrate
 
 dir_guard=@mkdir -p $(@D)
