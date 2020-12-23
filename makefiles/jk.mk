27087f437f741c126e3c7f6a43aa5ba9dcf6c4d6
jasperkoning <jasperkoning94@hotmail.com>
2020-12-23 16:04:36 +0100

    add swift

--- makefiles/jk.mk
+++ makefiles/jk.mk
@@ -14,7 +14,7 @@ CC = clang -isysroot $(JK_SDK)
 #CXX = /usr/bin/clang++-10 -isysroot $(JK_SDK)
 CXX = clang++ -isysroot $(JK_SDK)
 
-objects = $(foreach file, $(files), $(BUILD)/$(patsubst %.mm,%.o,$(patsubst %.xm,%.o,$(file:.m=.o))))
+objects = $(foreach file, $(files), $(BUILD)/$(patsubst %.mm,%.o,$(patsubst %.xm,%.o,$(patsubst %.swift,%.o,$(file:.m=.o)))))
 frameworkflags = $(foreach framework, $(frameworks), -framework $(framework))
 
 ifdef library
@@ -28,25 +28,37 @@ endif
 all: $(BINARYDIR)/$(name)
 
 white = \e[1;37m
-logos_print= \e[0;31m==>$(white)
-complile_print = @echo -e "\e[0;32m==>$(white) compiling $(patsubst %.xm,%.mm,$<) \e[0m"
+logos_print= \e[0;36m==>$(white)
+complile_print = @echo -e "\e[0;31m==>$(white) compiling $(patsubst %.xm,%.mm,$<) \e[0m"
 link_print = @echo -e "\e[0;33m==>$(white) linking $(patsubst %.xm,%.mm,$<) \e[0m"
+sign_print = @echo -e "\e[0;32m==>$(white) signing\e[0m"
 
-$(BINARYDIR)/$(name): $(mainlink) $(library)
+$(BINARYDIR)/$(name): $(mainlink) $(library) Makefile
 	@mkdir -p $(BINARYDIR)
 	$(link_print)
+ifneq ($(findstring .swift,$(files)),)
+	swiftc -sdk $(JK_SDK) $(mainlink) -o $@
+else
 	@$(CXX) -L$(vendor)/lib -L$(lib) $(libflags) $(frameworkflags) $(ldflags) $(mainlink) -o $@
+endif
+ifdef entitlements
+	$(sign_print)
+	@ldid -S$(entitlements) $@
+endif
 ifdef cpbinary
 	cp $(BINARYDIR)/$(name) .
 endif
 
+$(BUILD)/%.o: %.swift
+	swiftc -c -sdk $(JK_SDK) $(files) -o $@
+
 $(BUILD)/%.o: %.xm $(headers)
-	@$(shell if [ $< -nt $(BUILD)/$(patsubst %.xm,%.mm,$<) ]; then echo -e "\e[0;31m==>$(white) logos\e[0m" > `tty`; if ! $(THEOS)/bin/logos.pl $< > $(BUILD)/$(patsubst %.xm,%.mm,$<); then echo jkLOGOSFAILED; rm $(BUILD)/$(patsubst %.xm,%.mm,$<); fi; else echo no logos > `tty`;  fi)
+	@$(shell if [ $< -nt $(BUILD)/$(patsubst %.xm,%.mm,$<) ]; then echo -e "\e[0;36m==>$(white) logos\e[0m" > `tty`; if ! $(THEOS)/bin/logos.pl $< > $(BUILD)/$(patsubst %.xm,%.mm,$<); then echo jkLOGOSFAILED; rm $(BUILD)/$(patsubst %.xm,%.mm,$<); fi; else echo no logos > `tty`;  fi)
 	@cp $(BUILD)/$(patsubst %.xm,%.mm,$<) .
 	$(complile_print)
 	@$(CXX) $(flags) -c -I$(include) -I$(vendor)/include -I/usr/lib/llvm-10/include/c++/v1 $(patsubst %.xm,%.mm,$<) -o $@
 	@rm $(patsubst %.xm,%.mm,$<)
-	
+
 
 $(BUILD)/%.o: %.m $(headers)
 	@echo compiling $<
